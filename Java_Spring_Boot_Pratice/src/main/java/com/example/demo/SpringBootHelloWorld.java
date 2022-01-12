0424e422b5bebad8b7e0f7a84754b14d019b9e5c
Hong-Shao <joee0511@gmail.com>
2022-01-12 17:26:57 +0800

    add a new java-spring-boot practice

---
+++ Java_Spring_Boot_Pratice/src/main/java/com/example/demo/SpringBootHelloWorld.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/qq")
+// @RequestMapping(value = "/qq", method = RequestMethod.GET)
+public class SpringBootHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@GetMapping("/x")
+	public String hello(){
+		return "Hey, Spring Boot 的 Hello World !";
+	}
+	
+	@GetMapping("/indexx")
+	public String helloIndex(){
+		return "index";
+	}
+
+}
