f92b3abba2f74248ae9d95e65b6e12f0d5be8936
Hong-Shao <joee0511@gmail.com>
2022-01-16 22:33:07 +0800

    login and go cart is okay(tested successfully) may be complete(?), not tested

--- Taobao_Spider/run.py
+++ Taobao_Spider/run.py
@@ -4,6 +4,8 @@ import time
 import datetime
 import random
 
+specific_time = input('Specific time(24hr) (hh:mm:ss): ')
+specific_time = datetime.strptime(specific_time, '%H:%M:%S')
 
 chrome = webdriver.Chrome()
 
@@ -26,29 +28,40 @@ def login():
     except Exception as e:
         print(str(e))
 
-def go_cart_and_wait():
-    chrome.get(url='https://cart.taobao.com/cart.htm')
-    while True:
-        current = datetime.now()
-        # compute the time diff
-        # if time.diff(now, specific time).second > 180
-        if True:
-            time.sleep(random.randint(52, 78))
-            chrome.refresh()
-            pass
-        else:
-            break
-    
-def process_goods():
-    pass
+def go_cart_buy_and_pay():
+    chrome.get(url='https://world.taobao.com/cart/cart.htm')
 
-def pay():
-    pass
+    # click first store checkbutton
+    chrome.find_element(By.XPATH, 
+                        value='//*[@id="tp-bought-root"]/div[4]/div/table/tbody[1]/tr/td[1]/label/span[1]/input').
+    click()
+    
+    while True:
+        now = datetime.strptime(datetime.now(), '%H:%M:%S')
+        inteval = now - specific_time
+        if inteval.hour == 0 and inteval.minute == 0 and inteval.second == 2:
+            break
+        else: 
+            time.sleep(1)
+    
+    # submit the selected goods and go payment page
+    chrome.find_element(By.XPATH, 
+                        value='//*[@id="J_SmallSubmit"]').click()
+    # submit this order
+    chrome.find_element(By.XPATH, 
+                        value='//*[@id="submitOrderPC_1"]/div/a[2]').click()
+
+# check order is valid
+def check_order_details():
+    chrome.get('https://i.taobao.com/my_taobao.htm')
+    chrome.find_element(By.XPATH, value='//*[@id="bought"]').click()
+    chrome.find_element(By.XPATH, value='//*[@id="viewDetail"]').click()
+    print(chrome.find_element(By.XPATH, value='//*[@id="detail-panel"]/div/div[4]/div[2]/div/div/div/div[3]/div[1]/div[2]/span[3]/span[2]/span').text)
+
 
 # following https://www.cnblogs.com/sljsz/p/14887102.html 
 if __name__ == "__main__":
     login()
-    go_cart_and_wait()
-    process_goods()
-    pay()
+    go_cart_buy_and_pay()
+    check_order_details()
     print("Please check your order history details.")
